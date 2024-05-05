5294c4740dcd029363eb6072fc5743ba1fc799c6
Lupen <78322795+Lup3n@users.noreply.github.com>
2024-05-05 19:43:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Optional: Subtle flickering text effect 
 const elements = document.querySelectorAll('h2, p');
 
+window.location.href = "https://www.twitch.tv/videos/1100931082";
+
 setInterval(() => {
     elements.forEach(el => {
         el.style.opacity = Math.random() > 0.95 ? 0.5 : 1;
